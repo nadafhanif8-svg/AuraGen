d8b746f98f25edbb7ba66d1e4faf9fd08ebebf52
nadafhanif8-svg <nadafhanif8@gmail.com>
2026-02-01 18:43:49 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,4 +1,3 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
