894f81421e9ce511e2f75336941faf7de7c407da
nadafhanif8-svg <nadafhanif8@gmail.com>
2026-02-04 12:50:11 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,20 +1,22 @@
 import { auth } from "./firebase.js";
-import { signOut, onAuthStateChanged } from
-  "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import {
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-// Protect dashboard
+// 🔒 Protect dashboard + show user info
 onAuthStateChanged(auth, (user) => {
   if (!user) {
     window.location.href = "index.html";
+  } else {
+    document.getElementById("userEmail").innerText =
+      `Logged in as: ${user.email}`;
   }
 });
 
-// Logout (safe)
-const logoutBtn = document.getElementById("logoutBtn");
-if (logoutBtn) {
-  logoutBtn.addEventListener("click", () => {
-    signOut(auth).then(() => {
-      window.location.href = "index.html";
-    });
+// 🚪 Logout
+document.getElementById("logoutBtn").addEventListener("click", () => {
+  signOut(auth).then(() => {
+    window.location.href = "index.html";
   });
-}
+});
